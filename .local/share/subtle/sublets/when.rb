cf5ed8b99508f8190f32b970134ee5039e8a61e8
Andrea Brancaleoni <miwaxe@gmail.com>
2015-09-10 00:51:50 +0200

    when.rb: get only the first 4

--- .local/share/subtle/sublets/when.rb
+++ .local/share/subtle/sublets/when.rb
@@ -7,5 +7,5 @@ end
 on :run do |s|
   s.data = `when --past=0 --future=0 --language=en --no-header --nowrap_auto --nopaging --norows_auto --nostyled_output`.split("\n").map do |elem|
     elem.match(/[^\s]+\s+\d+\s+[^\s]+\s+\d+\s+(.*)/)[1]
-  end.select { |e| ! e.index '!'}.join(", ")
+  end.select { |e| ! e.index '!'}.first(4).join(", ")
 end
