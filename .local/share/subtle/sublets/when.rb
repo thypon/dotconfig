3d822b9af40a375039344e7aec227e1b7254b371
Andrea Brancaleoni <miwaxe@gmail.com>
2015-12-05 15:49:33 +0100

    when.rb: tries to fix Thread locks race conditions

--- .local/share/subtle/sublets/when.rb
+++ .local/share/subtle/sublets/when.rb
@@ -1,11 +1,14 @@
 # When sublet file
 # Created with sur-0.2
+# Put:
+#  @hourly when --past=0 --future=0 --language=en --no-header --nowrap_auto --nopaging --norows_auto --nostyled_output > .events
+# inside a cronjob
 configure :when do |s|
   s.interval = 300
 end
 
 on :run do |s|
-  s.data = `when --past=0 --future=0 --language=en --no-header --nowrap_auto --nopaging --norows_auto --nostyled_output`.split("\n").map do |elem|
+  s.data = File.readlines('~/.events').map do |elem|
     elem.match(/[^\s]+\s+\d+\s+[^\s]+\s+\d+\s+(.*)/)[1]
   end.select { |e| ! e.index '!'}.first(4).join(", ")
 end
