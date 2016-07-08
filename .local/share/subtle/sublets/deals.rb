67a56b4b12e18999a86b52f6eaa0cc4b38dfbf94
Andrea Brancaleoni <miwaxe@gmail.com>
2016-07-08 09:51:48 +0200

    New subtlet: deals.rb

---
+++ .local/share/subtle/sublets/deals.rb
@@ -0,0 +1,19 @@
+# When sublet file
+# Created with sur-0.2
+# Put:
+#  @hourly when --past=0 --future=0 --language=en --no-header --nowrap_auto --nopaging --norows_auto --nostyled_output > .events
+# inside a cronjob
+require 'json'
+
+configure :when do |s|
+  s.interval = 60
+end
+
+on :mouse_down do |s, x, y, button|
+  fork { `chromium #{s.deal['href']}` }
+end
+
+on :run do |s|
+  s.deal = JSON.parse(File.read("#{ENV['HOME']}/.deals"))
+  s.data = "#{s.deal['name']}, #{s.deal['price']}"
+end
